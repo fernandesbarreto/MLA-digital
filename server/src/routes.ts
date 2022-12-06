50b8947423b353688c56b91893bcf13363f3a12b
luizschmalz <luizeduardo2014schmalz@gmail.com>
2022-12-06 18:43:45 -0300

    Crud_Apoio

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import UserController from '@controllers/UserController'
+import ApoioController from '@controllers/ApoioController';
 
 const routes = express.Router();
 const userController = new UserController();
@@ -9,5 +10,11 @@ routes.get('/user', userController.get);
 routes.delete('/user/:id', userController.delete);
 routes.put('/user/:id', userController.update);
 
+const apoioController = new ApoioController();
+
+routes.post('/support', apoioController.create);
+routes.get('/support', apoioController.get);
+routes.delete('/support/:id', apoioController.delete);
+routes.put('/support/:id', apoioController.update);
 
 export default routes;
