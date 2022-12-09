fd61c122e0f58a1f3b88e81325b23cf115848da1
luizschmalz <luizeduardo2014schmalz@gmail.com>
2022-12-09 10:03:22 -0300

    page support finalized

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,10 +1,10 @@
 import LogoCITi from './logo.png';
-import fastMonion from './2 1.png';
-import sebraPe from './3 1.png';
-import feComercio from './4 1.png';
-import cashback from './5 1.png';
+import fastMonion from './fastmotion1.png';
+import sebraPe from './sebrae2.png';
+import feComercio from './fecomercio.png';
+import cashback from './cashback.png';
 import nivel from './6 1.png';
-import nivelAgmla from './7 1.png';
+import nivelAgmla from './nivelMgla1.png';
 import startUp from './8 1.png';
 import neoVentures from './9 1.png';
 
